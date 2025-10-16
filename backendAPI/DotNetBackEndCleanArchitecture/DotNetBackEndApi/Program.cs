842b8270a876e09740ee7e8854d5c185f69e9514
CY-Li <jimmy161688@gmail.com>
2025-10-16 23:30:44 +0800

    v1.0.0

--- backendAPI/DotNetBackEndCleanArchitecture/DotNetBackEndApi/Program.cs
+++ backendAPI/DotNetBackEndCleanArchitecture/DotNetBackEndApi/Program.cs
@@ -1,4 +1,3 @@
-
 using DomainEntityDTO.Common;
 using DotNetBackEndApi.Event;
 using DotNetBackEndApi.Filters;
@@ -12,6 +11,9 @@ using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Serilog;
 using Serilog.Events;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
@@ -19,49 +21,8 @@ Log.Logger = new LoggerConfiguration()
     .WriteTo.File("./logs/webapi_start_information_.log", rollingInterval: RollingInterval.Day)
     .CreateBootstrapLogger();
 
-#region serilog �Ȯɤ���
-//var _logger = new LoggerConfiguration()
-//    .MinimumLevel.Information()
-//    .Enrich.FromLogContext()
-//    .WriteTo.Map("ControllerName", "Other", (ControllerName, wt) => wt.File($"Logs/log-{ControllerName}.log"))
-//    .CreateLogger();
-
-//Log.Logger = new LoggerConfiguration()
-//    .WriteTo.("Name", "Other", (name, wt) => wt.File($"./logs/log-{name}.txt"))
-//    .CreateLogger();
-
-//Log.Logger = new LoggerConfiguration()
-//    .MinimumLevel.Information()
-//    .WriteTo.Logger(lc => lc
-//        .Filter.ByExcluding(e => e.Properties.ContainsKey("ControllerName"))
-//        .WriteTo.Sink(new ControllerLogSink()))
-//    .CreateLogger();
-
-//Log.Logger = new LoggerConfiguration()
-//    .MinimumLevel.Information()
-//    .WriteTo.Logger(lc => lc
-//        .Filter.ByIncludingOnly(e => e.Properties.ContainsKey("ControllerName") && e.Properties["ControllerName"].ToString() == "YourControllerName")
-//        .WriteTo.File("C:\\Logs\\YourControllerName\\{Date}.txt", rollingInterval: RollingInterval.Day))
-//    .CreateLogger();
-
-//Log.Logger = new LoggerConfiguration()
-//    .Enrich.FromLogContext()
-//    .WriteTo.Logger(lc => lc.Filter.ByExcluding(Matching.FromSource("Microsoft")))
-//    .WriteTo.Logger(lc => lc.Filter.ByIncludingOnly(evt => evt.Properties.TryGetValue("ControllerName", out var controller) && controller.ToString() == "HomeController")
-//        .WriteTo.File("logs/home-controller-.txt", rollingInterval: RollingInterval.Day))
-//    .WriteTo.Logger(lc => lc.Filter.ByIncludingOnly(evt => evt.Properties.TryGetValue("ControllerName", out var controller) && controller.ToString() == "OtherController")
-//        .WriteTo.File("logs/other-controller-.txt", rollingInterval: RollingInterval.Day))
-
-//Log.Logger = new LoggerConfiguration()
-//    .MinimumLevel.Information()
-//    .Enrich.FromLogContext()
-//    .WriteTo.Logger(lc => lc
-//        .Filter.ByIncludingOnly(evt => evt.Properties.TryGetValue("ControllerName", out var controller) && controller.ToString() == "HomeController")
-//        .WriteTo.File("logs/home-controller-.txt", rollingInterval: RollingInterval.Day))
-//    .WriteTo.Logger(lc => lc
-//        .Filter.ByIncludingOnly(evt => evt.Properties.TryGetValue("ControllerName", out var controller) && controller.ToString() == "OtherController")
-//        .WriteTo.File("logs/other-controller-.txt", rollingInterval: RollingInterval.Day))
-//    .CreateLogger();
+#region serilog
+// ... (omitted for brevity)
 #endregion
 try
 {
@@ -69,12 +30,6 @@ try
 
     var builder = WebApplication.CreateBuilder(args);
 
-    //�ϥ�Serilog
-    //builder.Host.UseSerilog((context, services, configuration) => configuration
-    //    .MinimumLevel.Information()
-    //    .WriteTo.Console()
-    //    .WriteTo.Sink(new ControllerLogSink())
-    //);
     builder.Host.UseSerilog((context, services, configuration) => configuration
         .MinimumLevel.Information()
         .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
@@ -94,76 +49,54 @@ try
 
     // Add services to the container.
     builder.Services.AddControllers();
-    builder.Services.Configure<ApiAppConfig>(builder.Configuration); // DI config�j���O
-    var mAppSetting = builder.Configuration.Get<ApiAppConfig>(); // �bprogram�̨ϥ�config�j���O
+    builder.Services.Configure<ApiAppConfig>(builder.Configuration); 
+    var mAppSetting = builder.Configuration.Get<ApiAppConfig>(); 
 
     // Add health checks
     builder.Services.AddHealthChecks()
         .AddCheck("self", () => HealthCheckResult.Healthy("API Gateway is running"));
 
-    //�Ӧa��O�]�w�VService����ƪ��]�w(�N�O�b�ϥθ�service�ɷ|�۰ʥ[�J��URL)
     builder.Services.AddHttpClient<HttpClientService>(c =>
     {
         c.BaseAddress = new Uri(mAppSetting.APIUrl);
         c.Timeout = TimeSpan.FromSeconds(600);
     });
 
-    #region Token �������filter
-    //�]�wTOKEM����
-    //builder.Services
-    //    .AddAuthentication(options =>
-    //    {
-    //        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    //        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    //        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    #region Token filter
+    builder.Services
+        .AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+         {
+             options.IncludeErrorDetails = true; 
 
-    //    })
-    //    .AddJwtBearer(options =>
-    //     {
-    //         // �����ҥ��ѮɡA�^�����Y�|�]�t WWW-Authenticate ���Y�A�o�̷|��ܥ��Ѫ��Բӿ��~��]
-    //         options.IncludeErrorDetails = true; // �w�]�Ȭ� true�A���ɷ|�S�O����
+             options.TokenValidationParameters = new TokenValidationParameters
+             {
+                 ValidateIssuer = mAppSetting.JwtSettings.ValidateIssuer,
+                 ValidIssuer = mAppSetting.JwtSettings.Issuer,
 
-    //         options.TokenValidationParameters = new TokenValidationParameters
-    //         {
-    //             // �z�L�o���ŧi�A�N�i�H�q "NAME" ����
-    //             //NameClaimType = @"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier",
-    //             // �z�L�o���ŧi�A�N�i�H�q "Role" ���ȡA�åi�� [Authorize] �P�_����
-    //             //RoleClaimType = @"http://schemas.microsoft.com/ws/2008/06/identity/claims/role",
+                 ValidateAudience = mAppSetting.JwtSettings.ValidateAudience,
+                 ValidAudience = mAppSetting.JwtSettings.Audience,
 
-    //             // ���� Issuer(�o���) (�@�볣�|)
-    //             ValidateIssuer = m_appSetting.JwtSettings.ValidateIssuer,
-    //             ValidIssuer = m_appSetting.JwtSettings.Issuer,
+                 ValidateIssuerSigningKey = mAppSetting.JwtSettings.ValidateIssuerSigningKey,
+                 IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(mAppSetting.JwtSettings.SecurityKey)),
 
-    //             // ���� Audience (�q�`���ӻݭn)
-    //             ValidateAudience = m_appSetting.JwtSettings.ValidateAudience,
-    //             ValidAudience = m_appSetting.JwtSettings.Audience,
-
-    //             // �p�G Token ���]�t key �~�ݭn���ҡA�@�볣�u��ñ���Ӥw
-    //             ValidateIssuerSigningKey = m_appSetting.JwtSettings.ValidateIssuerSigningKey,
-    //             // ���ӱq IConfiguration ���o
-    //             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(m_appSetting.JwtSettings.SecurityKey)),
-
-    //             // ���� Token �����Ĵ��� (�@�볣�|)
-    //             ValidateLifetime = m_appSetting.JwtSettings.ValidateLifetime,
-    //             ClockSkew = TimeSpan.Zero // �ɶ���A�N�L��
-    //         };
-    //     });
+                 ValidateLifetime = mAppSetting.JwtSettings.ValidateLifetime,
+                 ClockSkew = TimeSpan.Zero 
+             };
+         });
     #endregion
 
-    //�]�w�Ҧ�API���n�Ϊ�Filter
     builder.Services.AddMvc(options =>
     {
-        //�v�����ҡAJWT TOKEN
-        //options.Filters.Add(new AuthorizeFilter());
-        //options.Filters.Add(new BackEndApiAuthorizationFilter());
-
-        //�i�JAction�P���}�ɥi�H�gLOG
         options.Filters.Add(new ApiResourceFilter());
         options.Filters.Add(typeof(ApiActionFilter));
     });
 
-    //�]�w�ϥ�swagger���
-    //builder.Services.AddSwaggerGen();
     builder.Services.AddSwaggerGen(c =>
     {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "JwtDemo", Version = "v1" });
@@ -188,7 +121,6 @@ try
         c.EnableAnnotations();
     });
 
-    //�B�z�����D
     builder.Services.AddCors(options =>
     {
         options.AddPolicy("CorsPolicy",
@@ -200,21 +132,11 @@ try
             });
     });
 
-    //builder.Services.AddHangfire(config =>
-    //{
-    //    config.UseMemoryStorage();
-    //});
-    //builder.Services.AddHangfireServer();
-    //builder.Services.AddTransient<IBackgroundJobClient, BackgroundJobClient>();
-
     var app = builder.Build();
-    //app.UseHangfireDashboard();
     app.UseCors("CorsPolicy");
 
     app.UseSerilogRequestLogging();
 
-    // Configure the HTTP request pipeline.
-    //�Ȯɳ��|�}�Ҳ��Xswagger���
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
@@ -226,19 +148,10 @@ try
         app.UseSwaggerUI();
     }
 
-    //GET�ɦV�Ȯɤ���
-    //app.MapGet("/", () => @$"
-    //Environment: {Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}
-    //MySetting: {m_appSetting.JwtSettings.SecurityKey}");
+    app.UseAuthentication();
 
-    //�o�������O�]���ڲ{�b�S��https
-    //app.UseHttpsRedirection();// �ϥ��R�A�ɮסA�w�]�ؿ��O wwwroot
+    app.UseAuthorization();
 
-    app.UseAuthentication(); // ����
-
-    app.UseAuthorization(); // ���v
-
-    // Configure health check endpoint
     app.MapHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
     {
         ResponseWriter = HealthChecks.UI.Client.UIResponseWriter.WriteHealthCheckUIResponse
@@ -258,4 +171,3 @@ finally
 {
     Log.CloseAndFlush();
 }
-
